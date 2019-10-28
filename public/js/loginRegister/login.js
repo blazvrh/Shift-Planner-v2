4a3393f16e1b7261a7c1a41219e258cba3fac820
KrHo129 <krhodev@gmail.com>
2019-10-29 00:25:32 +0100

    redirect to https

--- public/js/loginRegister/login.js
+++ public/js/loginRegister/login.js
@@ -4,6 +4,26 @@ var imePoslovalnice = "";
 const loginBlock = document.getElementById("loginBlock");
 const logoutBlock = document.getElementById("logoutBlock");
 
+
+// redirects from http to https if needed
+// self invoked
+(function redirectToHttps () {
+    // get curent url address
+    const urlString = window.location.href;
+    // stop if on localhost
+    if (urlString.includes("localhost")) {
+        return;
+    }
+
+    let urlStringSecure = "";
+    
+    // check if it is on http
+    if (urlString.substr(0,4) === "http" && urlString.substr(0,5) !== "https") {
+        urlStringSecure = urlString.replace("http", "https");
+        window.location.href = urlStringSecure;
+    }
+}());
+
 // preveri če smo prijavljeni
 if (!userData) {
     if (sessionStorage.getItem("poslovalnica") !== null) {
