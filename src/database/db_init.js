d7d65a7ac6a345d65896ee6d0d8213a7e27224c0
KrHo129 <krhodev@gmail.com>
2020-03-05 17:43:59 +0100

    switch to backup

--- src/database/db_init.js
+++ src/database/db_init.js
@@ -1,23 +1,23 @@
 const mariadb = require('mariadb');
 
-const pool = mariadb.createPool({
-    host: 'remotemysql.com',
-    port: "3306",     
-    user:'lJJHApZs4s', 
-    password: 'IO6Y2JTKLN',
-    connectionLimit: 1,
-    database: "lJJHApZs4s"
-});
+// const pool = mariadb.createPool({
+//     host: 'remotemysql.com',
+//     port: "3306",     
+//     user:'lJJHApZs4s', 
+//     password: 'IO6Y2JTKLN',
+//     connectionLimit: 1,
+//     database: "lJJHApZs4s"
+// });
 
 // comment the upper and uncomment below in case of server down time
-// const pool = mariadb.createPool({
-//     host: 'sql7.freesqldatabase.com',
-//     port: "3306",     
-//     user:'sql7325565', 
-//     password: 'izhMz9nXtG',
-//     connectionLimit: 1,
-//     database: "sql7325565"
-// });
+const pool = mariadb.createPool({
+    host: 'sql7.freesqldatabase.com',
+    port: "3306",     
+    user:'sql7325565', 
+    password: 'izhMz9nXtG',
+    connectionLimit: 1,
+    database: "sql7325565"
+});
 
 const poolBackup = mariadb.createPool({
     host: 'sql7.freesqldatabase.com',
