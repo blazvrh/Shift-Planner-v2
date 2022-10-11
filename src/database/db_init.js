49f94cb085097bb379b69ae6b5d4edacc0627513
KrHo <krhodev@gmail.com>
2022-10-11 22:36:27 +0200

    removed test log

--- src/database/db_init.js
+++ src/database/db_init.js
@@ -13,7 +13,6 @@ require("dotenv").config();
 // });
 
 function getConnection() {
-  console.log(process.env.DATABASE_URL);
   const connection = mysql.createConnection(process.env.DATABASE_URL);
   return connection;
 }
