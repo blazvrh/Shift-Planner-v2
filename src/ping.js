d5fd389247ba0d1314ce8fb30d32e54f8fbd29fd
KrHo <krhodev@gmail.com>
2022-10-12 22:21:26 +0200

    ping interval increesed

--- src/ping.js
+++ src/ping.js
@@ -1,10 +1,8 @@
 var request = require("request");
 
 function ping() {
-  //   const intervalTime = 1000 * 60 * 10;
-  const intervalTime = 1000 * 60;
-    const url = "https://plan-dela.onrender.com/api/stay-awake";
-//   const url = "api/stay-awake";
+  const intervalTime = 1000 * 60 * 10;
+  const url = "https://plan-dela.onrender.com/api/stay-awake";
 
   const pingInterval = setInterval(() => {
     request(url, function (error, response, body) {
